ee754e9160cdfcb46d6584db7997f1c8ab4ec1e8
Diné Bennett <dine@ilovezoona.com>
2016-10-25 17:27:16 +0200

    Removed API key

---
+++ js/index.js
@@ -0,0 +1,36 @@
+var apiKey = 'fakeKey';
+var firebaseUrl = 'https://year-end-d85ea.firebaseio.com/rsvp.json?auth=' + apiKey;
+
+$(".submit-button").click(function () {
+    $name = $("#name").val().trim();
+    $partner = $("#partner").val();
+    $transport = $("#transport").val();
+    $meal = $("#meal").val();
+    console.log($name + " " + $partner + " " + $transport + " " + $meal);
+    if ($meal === null) {
+        $meal = "none";
+    }
+    if (($name.length < 1) || ($partner === null) || ($transport === null)) {
+        $('#myModal').modal('show');
+    } else {
+        $(".submit-button").text("Submitting...");
+        $(".submit-button").prop("disabled", true);
+        var person = {
+            "name": $name,
+            "partner": $partner,
+            "transport": $transport,
+            "meal": $meal,
+        };
+        $.post(firebaseUrl, JSON.stringify(person))
+            .done(function (msg) {
+                $(".container").toggleClass("flipped");
+                $(".back").show();
+                $(".front").hide();
+                $(".thank-you").text("THANK YOU " + ($name.toUpperCase().split(" ")[0]));
+                confetti();
+            })
+            .fail(function (xhr, status, error) {
+                $('#failModal').modal('show');
+            });
+    }
+});
